793441f2e23f685d194528902281fe3873f366cd
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2022-10-26 12:22:44 +0700

    feat: update

--- controllers/UserController/UserController.go
+++ controllers/UserController/UserController.go
@@ -63,3 +63,42 @@ func Store() gin.HandlerFunc {
         c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "message": "Success", "data": user})
     }
 }
+
+func Update() gin.HandlerFunc {
+    return func(c *gin.Context) {
+
+        type payloadType struct {
+            Name            string          `json:"name,omitempty" validate:"required"`
+            Email           string          `json:"email,omitempty" validate:"required"`
+            UserRoleId      uint            `json:"user_role_id,omitempty" validate:"required"`
+        }
+
+        payload := payloadType{}
+
+        db := configs.GetDB()
+        userId := c.Param("userId")
+        user := models.User{}
+
+        if err := c.BindJSON(&payload); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": "Invalid data", "data": nil})
+            return
+        }
+
+        result := db.Find(&user, userId)
+        if result.Error != nil {
+            c.JSON(http.StatusNotFound, gin.H{"code": http.StatusNotFound, "message": "Data not found", "data": nil})
+            return
+        }
+
+        user.Name = payload.Name
+        user.Email = payload.Email
+        user.UserRoleId = payload.UserRoleId
+        result = db.Save(&user)
+
+        if result.Error != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": "Unable to update data", "data": nil})
+            return
+        }
+        c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "message": "Success", "data": user})
+    }
+}
--- routes/Api.go
+++ routes/Api.go
@@ -9,4 +9,5 @@ func Api(router *gin.Engine) {
     router.GET("/user", UserController.Index())
     router.GET("/user/:userId", UserController.Show())
     router.POST("/user", UserController.Store())
+    router.PUT("/user/:userId", UserController.Update())
 }
